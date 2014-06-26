aa528051e11c627072fa4229bf37328f74bb96aa
Mitchell O'Flaherty-Sproul <073208o@acadiau.ca>
2014-06-26 08:08:15 -0300

    Cleaned up run_ttide.py. Runs a bunch of different test cases.

--- run_ttide.py
+++ run_ttide.py
@@ -49,13 +49,6 @@ data= ncdatasort(data)
 #testel=np.genfromtxt('kiti_118836.csv', dtype=None, delimiter=',')
 #print np.absolute(data['zeta'][199:-1,118836]-testel).min()
 #print np.absolute(data['zeta'][199:-1,118836]-testel).max()
-#[nameu, fu, tidecon, xout]=tt.t_tide(data['zeta'][199:-1,118836])
-#[nameu, fu, tidecon, xout]=tt.t_tide(data['zeta'][199:-1,118836],data['time'][0],dt=.5)
-
-#[nameu, fu, tidecon, xout]=tt.t_tide(data['ua'][199:-1,149999]+1j*data['va'][199:-1,149999],stime=data['time'][0])
-#[nameu, fu, tidecon2, xout]=tt.t_tide(data['ua'][199:-1,149999]+1j*data['va'][199:-1,149999],stime=data['time'][0],dt=0.5)
-
-#[nameu, fu, tidecon, xout]=tt.t_tide(data['zeta'][199:-1,118836],stime=data['time'][0],dt=0.5,lat=data['nodell'][118836,1])
 
 
 
@@ -63,10 +56,98 @@ data= ncdatasort(data)
 
 
 
-#[nameu, fu, tidecon2, xout]=tt.t_tide(data['zeta'][199:-1,118836],stime=data['time'][0])
+
+#[nameu, fu, tidecon, xout]=tt.t_tide(data['zeta'][199:-1,118836],dt=1.555)
+print 
+print '/\   Scalar test (errcalc=linear)'
+print 
+#[nameu, fu, tidecon, xout]=tt.t_tide(data['ua'][199:-1,149999]+1j*data['va'][199:-1,149999],errcalc='linear')
+print 
+print '/\   Complex test (errcalc=linear)'
+print 
+
+
+#kill
+
+[nameu, fu, tidecon, xout]=tt.t_tide(data['zeta'][199:-1,118836],errcalc='wboot')
+print 
+print '/\   Scalar test (errcalc=wboot)'
+print 
+[nameu, fu, tidecon, xout]=tt.t_tide(data['ua'][199:-1,149999]+1j*data['va'][199:-1,149999],errcalc='wboot')
+print 
+print '/\   Complex test (errcalc=wboot)'
+print 
+
+
+
+[nameu, fu, tidecon, xout]=tt.t_tide(data['zeta'][199:-1,118836],stime=data['time'][0],lat=data['nodell'][118836,1],constitnames=np.array([['M2  ']]))
+print 
+print '/\   Scalar test (stime,lat,con=m2)'
+print 
+[nameu, fu, tidecon, xout]=tt.t_tide(data['ua'][199:-1,149999]+1j*data['va'][199:-1,149999],stime=data['time'][0],lat=data['uvnodell'][149999,1],constitnames=np.array([['M2  ']]))
+print 
+print '/\   Complex test (stime,lat,con=m2)'
+print 
+
+
+
 [nameu, fu, tidecon, xout]=tt.t_tide(data['zeta'][199:-1,118836],stime=data['time'][0],lat=data['nodell'][118836,1])
-[nameu, fu, tidecon, xout]=tt.t_tide(data['zeta'][199:-1,118836],stime=data['time'][0],lat=data['nodell'][118836,1],constitnames=np.array([['M2  '],['N2  ']]))
-#print tp.t_predic(data['time'][0:10],nameu,fu,tidecon)
+print 
+print '/\   Scalar test (stime,lat)'
+print
+[nameu, fu, tidecon, xout]=tt.t_tide(data['ua'][199:-1,149999]+1j*data['va'][199:-1,149999],stime=data['time'][0],lat=data['uvnodell'][149999,1])
+print 
+print '/\   Complex test (stime,lat)'
+print 
+
+
+
+[nameu, fu, tidecon, xout]=tt.t_tide(data['zeta'][199:-1,118836],stime=data['time'][0],dt=0.5,lat=data['nodell'][118836,1])
+print 
+print '/\   Scalar test (stime,dt,lat)'
+print 
+[nameu, fu, tidecon, xout]=tt.t_tide(data['ua'][199:-1,149999]+1j*data['va'][199:-1,149999],stime=data['time'][0],dt=0.5,lat=data['uvnodell'][149999,1])
+print 
+print '/\   Complex test (stime,dt,lat)'
+print 
+
+
+
+[nameu, fu, tidecon, xout]=tt.t_tide(data['zeta'][199:-1,118836],stime=data['time'][0],dt=0.5)
+print 
+print '/\   Scalar test (stime,dt)'
+print 
+[nameu, fu, tidecon, xout]=tt.t_tide(data['ua'][199:-1,149999]+1j*data['va'][199:-1,149999],stime=data['time'][0],dt=0.5)
+print 
+print '/\   Complex test (stime,dt)'
+print 
+
+
+
+[nameu, fu, tidecon, xout]=tt.t_tide(data['zeta'][199:-1,118836],stime=data['time'][0])
+print 
+print '/\   Scalar test (stime)'
+print 
+[nameu, fu, tidecon, xout]=tt.t_tide(data['ua'][199:-1,149999]+1j*data['va'][199:-1,149999],stime=data['time'][0])
+print 
+print '/\   Complex test (stime)'
+print 
+
+
+
+[nameu, fu, tidecon, xout]=tt.t_tide(data['zeta'][199:-1,118836])
+print 
+print '/\   Scalar test'
+print 
+[nameu, fu, tidecon, xout]=tt.t_tide(data['ua'][199:-1,149999]+1j*data['va'][199:-1,149999])
+print 
+print '/\   Complex test'
+print 
+
+
+
+
+
 
 
 
