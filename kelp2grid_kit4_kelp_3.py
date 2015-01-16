6b35a9ec589dd5676a0eb2e2b1384a7095913abf
Mitchell O'Flaherty-Sproul <073208o@acadiau.ca>
2015-01-16 14:26:15 -0400

    kelp2grid_kit4_kelp_3.py merges two kelp files and then makes a elementlist/ cage file.

---
+++ kelp2grid_kit4_kelp_3.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+Front Matter
+=============
+
+Created on 
+
+Author: Mitchell O'Flaherty-Sproul
+
+Requirements
+===================================
+Absolutely Necessary:
+
+* Numpy
+* SciPy
+* Matplotlib 
+
+
+Optional, but recommended:
+
+Functions
+=========
+"""
+#load modules
+import numpy as np
+import scipy.io as sio
+import matplotlib.pyplot as plt
+from math import pi
+from datatools import *
+from gridtools import *
+from misctools import *
+import scipy as sp
+import matplotlib as mpl
+np.set_printoptions(precision=8,suppress=True,threshold=np.nan)
+import sys
+
+
+
+
+
+# Define names and types of data
+name='kit4_kelp_newbathy_test'
+grid='kit4_kelp'
+#name='kit4_45days_3'
+#grid='kit4'
+regionname='kit4_4island'
+datatype='2d'
+starttime=0
+plotspeed=False
+
+
+### load the .nc file #####
+data = loadnc('runs/' +grid+'/' + name + '/output/',singlename=grid + '_0001.nc')
+print 'done load'
+data = ncdatasort(data,trifinder=True)
+print 'done sort'
+
+
+kelp=np.genfromtxt('data/misc/kelplocations.dat')
+kelp2=np.genfromtxt('data/misc/kelplocations_ll.dat')
+
+kelp=np.vstack([kelp,kelp2])
+
+maxdepth=20
+mindepth=0
+
+
+savepath='figures/png/' + grid + '_' + datatype + '/misc/'
+if not os.path.exists(savepath): os.makedirs(savepath)
+
+region=regions(regionname)
+nidx=get_nodes(data,region)
+eidx=get_elements(data,region)
+
+
+host=data['trigrid_finder'].__call__(kelp[:,0],kelp[:,1])
+#host=np.unique(host)
+#host=host[host !=-1]
+
+newhost=host
+
+
+    
+        
+newhost=np.unique(newhost)
+newhost=newhost[newhost !=-1]     
+
+host_lt_depth=np.where((data['uvh'][newhost]<=maxdepth) & (data['uvh'][newhost]>=mindepth))[0]
+newhostbool=np.zeros(shape=newhost.shape,dtype=bool)
+newhostbool[host_lt_depth]=True
+
+newhost=newhost[host_lt_depth.flatten()]
+
+tempdic={}
+tempdic['kelp_elements']=newhost
+#sio.savemat('kelp_elements_kit4.mat',mdict=tempdic)
+
+
+np.savetxt('data/grid_stuff/kelpnodes_'+grid+'.dat',newhost+1,fmt='%i')
+
+drag=np.zeros([newhost.shape[0],])+0.018
+depth=np.zeros([newhost.shape[0],])+40
+
+fvcom_savecage('data/cage_files/'+grid+'_cage_'+("%d"%mindepth)+'m_'+("%d"%maxdepth)+'m.dat',newhost+1,drag,depth)
+
+trihost=np.zeros([data['uvnodell'].shape[0],])
+trihost[newhost]=1
+
+plt.close()
+plt.tripcolor(data['trigrid'],trihost)
+plt.colorbar()
+plt.grid()
+plt.axis(region['region'])
+plt.title('Locations with kelp')
+plt.savefig(savepath +grid+'_'+regionname+ 'kelp_host_locations_3.png',dpi=1200)
+plt.close()
+
