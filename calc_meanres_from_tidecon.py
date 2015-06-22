4bfa5fa22d1da1dc8d6a27e3a89cb3c2458e74ce
Mitchell O'Flaherty-Sproul <073208o@acadiau.ca>
2015-06-22 19:51:50 -0300

    Filename changes.

--- calc_meanres_from_tidecon.py
+++ calc_meanres_from_tidecon.py
@@ -16,10 +16,10 @@ from t_tide import t_tide
 from t_predic import t_predic
 
 # Define names and types of data
-name='kit4_kelp_20m_drag_0.018'
-grid='kit4_kelp'
-datatype='2d'
-starttime=384
+name='sfm6_musq2_all_cages'
+grid='sfm6_musq2'
+datatype='3d'
+starttime=0
 
 
 ### load the .nc file #####
@@ -30,7 +30,7 @@ print 'done sort'
 
 
 
-ttidein=np.load('data/ttide/'+grid+'_'+name+'_'+datatype+'_uv_all.npy')
+ttidein=np.load('data/ttide/'+grid+'_'+name+'_'+datatype+'_uv_layer_19_all.npy')
 ttidein=ttidein[()]
 
 tidecon=ttidein['tidecon']
@@ -44,8 +44,8 @@ resv=np.empty((tidecon.shape[0],len(data['time'][starttime:])))
 for j in range(0,tidecon.shape[0]):
     print ("%d"%j)+"              "+("%f"%(j/tidecon.shape[0]*100)) 
     tpre=t_predic(data['time'][starttime:],nameu,freq,tidecon[j,:,:])
-    resu[j,:]=data['ua'][starttime:,j]-np.real(tpre).flatten()
-    resv[j,:]=data['va'][starttime:,j]-np.imag(tpre).flatten()
+    resu[j,:]=data['u'][starttime:,19,j]-np.real(tpre).flatten()
+    resv[j,:]=data['v'][starttime:,19,j]-np.imag(tpre).flatten()
 
 
 resmean={}
@@ -53,7 +53,7 @@ resmean['resumean']=resu.mean(axis=1)
 resmean['resvmean']=resv.mean(axis=1)
 
 
-np.save('data/ttide/'+grid+'_'+name+'_'+datatype+'_uv_all_mean.npy',resmean)
+np.save('data/ttide/'+grid+'_'+name+'_'+datatype+'_uv_layer_19_all_mean.npy',resmean)
 
 
 
