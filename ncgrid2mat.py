29a3f6ee2a08d071da9f70792ea51674e18bfea9
Mitchell O'Flaherty-Sproul <073208o@acadiau.ca>
2015-07-24 15:30:26 -0300

    Save baroclinic to mat files. find_slacktide_offset finds out the slacktide timing difference between two locations.

--- ncgrid2mat.py
+++ ncgrid2mat.py
@@ -37,10 +37,10 @@ import sys
 
 
 # Define names and types of data
-name='kit4_45days_3'
-grid='kit4'
+name='kit4_kelp_baroclinic_drag_0.007'
+grid='kit4_kelp'
 datatype='2d'
-starttime=1008
+starttime=384
 interpheight=1
 
 ### load the .nc file #####
@@ -69,32 +69,86 @@ tempdic['siglev']=data['siglev'][:,0]
 sio.savemat('data/ncgrid2mat/'+grid +'_basic.mat',mdict=tempdic)
 
 
-#tempdic['ua']=data['ua'][starttime:,:]
-#tempdic['va']=data['va'][starttime:,:]
-#tempdic['time']=data['time'][starttime:]
-#tempdic['zeta']=data['zeta'][starttime:,:]
-#sio.savemat('data/ncgrid2mat/'+grid +'_'+name+'_currents.mat',mdict=tempdic)
+tempdic['ua']=data['ua'][starttime:,:]
+tempdic['va']=data['va'][starttime:,:]
+tempdic['time']=data['time'][starttime:]
+tempdic['zeta']=data['zeta'][starttime:,:]
+sio.savemat('data/ncgrid2mat/'+grid +'_'+name+'_currents.mat',mdict=tempdic)
 
-#tempdic={}
-#tempdic['trigrid']=data['nv']+1
-#tempdic['lon']=data['lon']
-#tempdic['lat']=data['lat']
-#tempdic['lonc']=data['uvnodell'][:,0]
-#tempdic['latc']=data['uvnodell'][:,1]
-#tempdic['x']=data['x']
-#tempdic['y']=data['y']
-#tempdic['xc']=data['uvnode'][:,0]
-#tempdic['yc']=data['uvnode'][:,1]
-#tempdic['h']=data['h']
-#tempdic['hc']= (data['h'][data['nv'][:,0]] + data['h'][data['nv'][:,1]] + data['h'][data['nv'][:,2]]) / 3.0
-#tempdic['siglay']=data['siglay'][:,0]
-#tempdic['siglev']=data['siglev'][:,0]
+tempdic={}
+tempdic['trigrid']=data['nv']+1
+tempdic['lon']=data['lon']
+tempdic['lat']=data['lat']
+tempdic['lonc']=data['uvnodell'][:,0]
+tempdic['latc']=data['uvnodell'][:,1]
+tempdic['x']=data['x']
+tempdic['y']=data['y']
+tempdic['xc']=data['uvnode'][:,0]
+tempdic['yc']=data['uvnode'][:,1]
+tempdic['h']=data['h']
+tempdic['hc']= (data['h'][data['nv'][:,0]] + data['h'][data['nv'][:,1]] + data['h'][data['nv'][:,2]]) / 3.0
+tempdic['siglay']=data['siglay'][:,0]
+tempdic['siglev']=data['siglev'][:,0]
 
-#tempdic['u_0']=data['u'][starttime:(starttime+30),0,:]
-#tempdic['u_19']=data['u'][starttime:(starttime+30),19,:]
-#tempdic['v_0']=data['v'][starttime:(starttime+30),0,:]
-#tempdic['v_19']=data['v'][starttime:(starttime+30),19,:]
-#sio.savemat('data/ncgrid2mat/'+grid +'_'+name+'_uv_currents.mat',mdict=tempdic)
+tempdic['u']=data['u'][starttime:(starttime+30),:,:]
+tempdic['v']=data['v'][starttime:(starttime+30),:,:]
+
+sio.savemat('data/ncgrid2mat/'+grid +'_'+name+'_uv_currents.mat',mdict=tempdic)
+
+
+tempdic={}
+tempdic['trigrid']=data['nv']+1
+tempdic['lon']=data['lon']
+tempdic['lat']=data['lat']
+tempdic['lonc']=data['uvnodell'][:,0]
+tempdic['latc']=data['uvnodell'][:,1]
+tempdic['x']=data['x']
+tempdic['y']=data['y']
+tempdic['xc']=data['uvnode'][:,0]
+tempdic['yc']=data['uvnode'][:,1]
+tempdic['h']=data['h']
+tempdic['hc']= (data['h'][data['nv'][:,0]] + data['h'][data['nv'][:,1]] + data['h'][data['nv'][:,2]]) / 3.0
+tempdic['siglay']=data['siglay'][:,0]
+tempdic['siglev']=data['siglev'][:,0]
+
+tempdic['salinity']=data['salinity'][starttime:(starttime+30),:,:]
+tempdic['temp']=data['temp'][starttime:(starttime+30),:,:]
+
+sio.savemat('data/ncgrid2mat/'+grid +'_'+name+'_temp_sal.mat',mdict=tempdic)
+
+
+tempdic={}
+tempdic['trigrid']=data['nv']+1
+tempdic['lon']=data['lon']
+tempdic['lat']=data['lat']
+tempdic['lonc']=data['uvnodell'][:,0]
+tempdic['latc']=data['uvnodell'][:,1]
+tempdic['x']=data['x']
+tempdic['y']=data['y']
+tempdic['xc']=data['uvnode'][:,0]
+tempdic['yc']=data['uvnode'][:,1]
+tempdic['h']=data['h']
+tempdic['hc']= (data['h'][data['nv'][:,0]] + data['h'][data['nv'][:,1]] + data['h'][data['nv'][:,2]]) / 3.0
+tempdic['siglay']=data['siglay'][:,0]
+tempdic['siglev']=data['siglev'][:,0]
+
+tempdic['km']=data['km'][starttime:(starttime+30),:,:]
+tempdic['kh']=data['kh'][starttime:(starttime+30),:,:]
+
+sio.savemat('data/ncgrid2mat/'+grid +'_'+name+'_temp_sal.mat',mdict=tempdic)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
