794136fdbf614c3c63ff80f8e72aecc298746d69
Mitchell O'Flaherty-Sproul <073208o@acadiau.ca>
2015-07-23 23:51:32 -0300

    Plot power corr with neighbouring nodes.

---
+++ plot_power_corr.py
@@ -0,0 +1,57 @@
+from __future__ import division
+import matplotlib as mpl
+import scipy as sp
+from datatools import *
+from gridtools import *
+from plottools import *
+from projtools import *
+from misctools import *
+import matplotlib.tri as mplt
+import matplotlib.pyplot as plt
+#from mpl_toolkits.basemap import Basemap
+import os as os
+import sys
+np.set_printoptions(precision=8,suppress=True,threshold=np.nan)
+from scipy.stats.stats import pearsonr 
+
+
+# Define names and types of data
+name='voucher_2d_repmonth'
+grid='voucher'
+datatype='2d'
+regionname='mp'
+
+
+### load the .nc file #####
+data = loadnc('runs/'+grid+'/'+name+'/output/',singlename=grid + '_0001.nc')
+print 'done load'
+data = ncdatasort(data)
+print 'done sort'
+
+region=regions(regionname)
+eidx=get_elements(data,region)
+
+savepath='figures/png/' + grid + '_' + datatype + '/misc/'
+if not os.path.exists(savepath): os.makedirs(savepath)
+
+speed=speeder(data['ua'],data['va'])**3
+
+elecor=np.empty((data['nele'],))
+
+for i in range(data['nele']):
+    print i
+    tcor=np.array([pearsonr(speed[:,i],speed[:,x])[0] for x in data['nbe'][i,:]])
+    elecor[i]=np.nanmean(tcor[data['nbe'][i,:]!=-1])
+    
+    
+elecor[np.isnan(elecor)]=0    
+
+clim=np.percentile(elecor[eidx],[10,90])
+
+f=plt.figure()
+ax=f.add_axes([.125,.1,.775,.8])
+triax=ax.tripcolor(data['trigrid'],elecor,vmin=clim[0],vmax=clim[1])
+prettyplot_ll(ax,setregion=region,cb=triax,cblabel='Power Corr')
+f.savefig(savepath + grid + '_' +name+ '_'+regionname +'_power_corr.png',dpi=600)
+
+plt.close(f)
