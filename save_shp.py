964b846de5a431af85ccc2a4632e6150805615a8
Mitchell O'Flaherty-Sproul <073208o@acadiau.ca>
2015-12-11 15:38:51 -0400

    Code to save point shape files

--- save_shp.py
+++ save_shp.py
@@ -25,16 +25,15 @@ region=regions(regionname)
 data = loadnc('runs/'+grid+'/'+name+'/output/',singlename=grid+'_0001.nc')
 data =ncdatasort(data)
 
-save_poly_shp(data,'uvh','data/test/'+grid+'_depth.shp')
-
+#save_poly_shp(data,'uvh','data/test/'+grid+'_depth.shp')
 
 
 
 filename='data/test/'+grid+'_speed_dir_100.shp'
 epsg_in=4326
 
-lon = data['lon']
-lat = data['lat']
+lon = data['uvnodell'][:,0]
+lat = data['uvnodell'][:,1]
 trinodes = data['nv']
 speed_100=speeder(data['ua'][100,:],data['va'][100,:])
 dir_100=np.arctan2(data['va'][100,:],data['ua'][100,:])
@@ -45,35 +44,54 @@ shapeData = driver.CreateDataSource(filename)
 
 spatialRefi = osr.SpatialReference()
 spatialRefi.ImportFromEPSG(epsg_in)
-lyr = shapeData.CreateLayer("poly_layer", spatialRefi, ogr.wkbPolygon )
+lyr = shapeData.CreateLayer("point_layer", spatialRefi, ogr.wkbPoint )
 
 #var is just a rdm string?
 lyr.CreateField(ogr.FieldDefn('speed_100', ogr.OFTReal))
 
-cnt = 0
-for row in trinodes:
-    val1 = -999
-    ring = ogr.Geometry(ogr.wkbLinearRing)
-    for val in row:
-        if val1 == -999:
-            val1 = val
-        ring.AddPoint(lon[val], lat[val])
-    #Add 1st point to close ring
-    ring.AddPoint(lon[val1], lat[val1])
 
-    poly = ogr.Geometry(ogr.wkbPolygon)
-    poly.AddGeometry(ring)
+for i,row in enumerate(trinodes):
+    val1 = -999
+    pt = ogr.Geometry(ogr.wkbPoint)
+    #Add 1st point to close ring
+    pt.AddPoint(lon[i], lat[i])
 
     #Now add field values from array
     feat = ogr.Feature(lyr.GetLayerDefn())
-    feat.SetGeometry(poly)
-    feat.SetField(varLabel, float(var[cnt]))
+    feat.SetGeometry(pt)
+    feat.SetField('speed_100', float(speed_100[i]))
 
     lyr.CreateFeature(feat)
     feat.Destroy()
-    poly.Destroy()
+    pt.Destroy()
+
+    val1 = -999 
+
+
+
+
+
+
+
+#var is just a rdm string?
+lyr.CreateField(ogr.FieldDefn('dir_100', ogr.OFTReal))
+
+
+for i,row in enumerate(trinodes):
+    val1 = -999
+    pt = ogr.Geometry(ogr.wkbPoint)
+    #Add 1st point to close ring
+    pt.AddPoint(lon[i], lat[i])
+
+    #Now add field values from array
+    feat = ogr.Feature(lyr.GetLayerDefn())
+    feat.SetGeometry(pt)
+    feat.SetField('dir_100', float(dir_100[i]))
+
+    lyr.CreateFeature(feat)
+    feat.Destroy()
+    pt.Destroy()
 
     val1 = -999
-    cnt += 1
     
 shapeData.Destroy()
