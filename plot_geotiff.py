60ad7019aa9408af78bd6335cc1f499df9033208
Mitchell O'Flaherty-Sproul <073208o@acadiau.ca>
2016-01-19 14:46:47 -0400

    enav plotting

--- plot_geotiff.py
+++ plot_geotiff.py
@@ -2,9 +2,28 @@ from osgeo import osr, gdal
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import LinearSegmentedColormap
+from datatools import *
+from gridtools import *
+from misctools import *
+from plottools import *
+from projtools import *
+import LatLon
+
 
 gdal.UseExceptions()
-ds = gdal.Open('data/misc/vhfr_obs/misc/3481_Geotiff.tif')
+ds = gdal.Open('data/enav/3481_Geotiff.tif')
+band = ds.GetRasterBand(1)
+ct=band.GetColorTable()
+elevation = ds.ReadAsArray()
+cb=np.array([])
+for i in range(ct.GetCount()):
+    cb=np.append(cb,ct.GetColorEntry(i)[:])
+cb=cb.reshape(-1,4)
+
+mycmap=LinearSegmentedColormap.from_list('my_colormap',cb[:13,:]/256,13)
+
+gdal.UseExceptions()
+ds = gdal.Open('data/enav/First_Narrows.tif')
 band = ds.GetRasterBand(1)
 ct=band.GetColorTable()
 elevation = ds.ReadAsArray()
@@ -26,6 +45,7 @@ y1 = y0 + dy * nrows
 # But we really need lon/lat:
 old_cs= osr.SpatialReference()
 old_cs.ImportFromWkt(ds.GetProjectionRef())
+
 # create the new coordinate system (This might not be exactly what we want to do because we know the reference system is NAD83 (Not WGS84) but I did a test and you get the lon,lat values that are given in the geotiff for the corners.
  
 wgs84_wkt = """
@@ -39,8 +59,12 @@ GEOGCS["WGS 84",
     UNIT["degree",0.01745329251994328,
         AUTHORITY["EPSG","9122"]],
     AUTHORITY["EPSG","4326"]]"""
+    
+
 new_cs = osr.SpatialReference()
 new_cs.ImportFromWkt(wgs84_wkt)
+
+
 
 # create a transform object to convert between coordinate systems
 transform = osr.CoordinateTransformation(old_cs,new_cs) 
@@ -59,21 +83,27 @@ latlongBL = transform.TransformPoint(minx,miny)
 latlongBR = transform.TransformPoint(maxx,miny)
 latlongTL = transform.TransformPoint(minx,maxy)
 latlongTR = transform.TransformPoint(maxx,maxy) 
+extent=[latlongBL[0],latlongTR[0],latlongBL[1],latlongTR[1]]
 
 elevation2 = np.array(gt)
 
-cb=np.array([])
-for i in range(ct.GetCount()):
-    cb=np.append(cb,ct.GetColorEntry(i)[:])
-cb=cb.reshape(-1,4)
+#cb=np.array([])
+#for i in range(ct.GetCount()):
+    #cb=np.append(cb,ct.GetColorEntry(i)[:])
+#cb=cb.reshape(-1,4)
 
-mycmap=LinearSegmentedColormap.from_list('my_colormap',cb[:13,:]/256,13)
+#mycmap=LinearSegmentedColormap.from_list('my_colormap',cb[:13,:]/256,13)
 
 
+#llstr=['123 23 36.76 W','49 12 40.19 N' , '122 59 5.95 W', '49 24 0.45 N']
+#LL=LatLon.string2latlon(llstr[1],llstr[0], 'd% %m% %S% %h')
+#UR=LatLon.string2latlon(llstr[3],llstr[2], 'd% %m% %S% %h')
+#extent=[-LL.lon.decimal_degree,-UR.lon.decimal_degree,LL.lat.decimal_degree,UR.lat.decimal_degree]
 
-plt.
+plt.imshow(ds.ReadAsArray().mean(axis=0), extent=extent,cmap=mycmap,vmax=13)
 # zoom in on our region
 plt.axis([-123.18,-123.08,49.28,49.33])
+plotcoast(plt.gca(),filename='pacific_harbour.nc',color='r')
 plt.show()
 
 
