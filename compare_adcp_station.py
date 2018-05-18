7ae65517d294df4e51698ea62e394acae6b9d090
Mitchell O'Flaherty-Sproul <073208o@acadiau.ca>
2018-05-18 02:15:16 -0300

    Added code to extract adcp data from stationfile and compare it with observations

---
+++ compare_adcp_station.py
@@ -0,0 +1,142 @@
+from __future__ import division,print_function
+import matplotlib as mpl
+import scipy as sp
+from folderpath import *
+from datatools import *
+from gridtools import *
+from plottools import *
+from projtools import *
+from stattools import *
+import interptools as ipt
+import matplotlib.tri as mplt
+import matplotlib.pyplot as plt
+#from mpl_toolkits.basemap import Basemap
+import os as os
+import sys
+np.set_printoptions(precision=8,suppress=True,threshold=np.nan)
+import pandas as pd
+import netCDF4 as n4
+import copy
+import matplotlib.dates as dates
+from collections import OrderedDict
+import ttide
+
+
+# Define names and types of data
+name='sjh_lr_v1_year_origbc_wet_hfx100'
+#name='sjh_hr_v3_year_wet'
+grid='sjh_lr_v1'
+datatype='2d'
+print(name)
+
+
+filenames=glob.glob('/mnt/drive_1/obs_data/east/adcp/*.npy')
+filenames.sort()
+loadpath='{}/{}_{}/adcp/{}/'.format(datapath,grid,datatype,name)
+dt=dates.datetime.timedelta(0,60)
+
+#savepath='{}/{}_{}/adcp/{}/'.format(datapath,grid,datatype,name)
+#if not os.path.exists(savepath): os.makedirs(savepath)
+
+
+for i,filename in enumerate(filenames):
+    print('='*80)
+    print(i)
+    print(filename)
+    
+    adcp = np.load(filename)
+    adcp = adcp[()]
+    try:
+        model = np.load('{}ADCP_{}_model_ministation.npy'.format(loadpath,adcp['metadata']['ADCP_number']))
+        model = model[()]
+    except:
+        continue
+    
+    obs={}
+    mod={}    
+    
+    lvl=np.array([2,5,10])
+    obs['rtime']=dates.datestr2num(adcp['time']['Times'])
+    obs['rv']=np.empty((len(obs['rtime']),len(lvl)))
+    obs['ru']=np.empty((len(obs['rtime']),len(lvl)))
+    for i in range(len(obs['rtime'])):
+        obs['rv'][i,]=ipt.interp1d(adcp['pres']['topheight'][i,:],adcp['data']['north_vel'][i,:],lvl)
+        obs['ru'][i,]=ipt.interp1d(adcp['pres']['topheight'][i,:],adcp['data']['east_vel'][i,:],lvl)
+
+    mod['rtime']=model['time']
+    mod['rv']=np.empty((len(mod['rtime']),len(lvl)))
+    mod['ru']=np.empty((len(mod['rtime']),len(lvl)))  
+    for i in range(len(mod['rtime'])):
+        mod['rv'][i,]=ipt.interp1d(-1*model['siglay']*(model['zeta'][i]+model['h']),model['v'][i,],lvl)
+        mod['ru'][i,]=ipt.interp1d(-1*model['siglay']*(model['zeta'][i]+model['h']),model['u'][i,],lvl)
+
+    
+    
+    
+
+    
+
+    timeshift=dates.drange(dates.num2date(obs['rtime'][0]),dates.num2date(obs['rtime'][-1]),dt)
+    for j,level in enumerate(lvl):
+        try:
+            print('calculating level {}'.format(level))
+            ov=ipt.interp1d(obs['rtime'],obs['rv'][:,j],timeshift)
+            ou=ipt.interp1d(obs['rtime'],obs['ru'][:,j],timeshift)
+            
+            mv=ipt.interp1d(mod['rtime'],mod['rv'][:,j],timeshift)
+            mu=ipt.interp1d(mod['rtime'],mod['ru'][:,j],timeshift)
+            
+            nidx=np.isnan(ov)
+            mv[nidx]=np.nan
+            mu[nidx]=np.nan
+        
+            r1v=residual_stats(mv[~nidx],ov[~nidx])
+            r1u=residual_stats(mu[~nidx],ou[~nidx])
+        
+            print('T_tide obs')
+            oout=ttide.t_tide(ou+1j*ov,dt=60.0/3600.0,stime=timeshift[0],lat=adcp['lat'],out_style=None)
+            osnr=0
+            clist=oout['nameu'][oout['snr']>2]
+            nsnr=len(clist)
+            while(osnr!=nsnr):
+                #print('looping')
+                oout2=ttide.t_tide(ou+1j*ov,dt=60.0/3600.0,stime=timeshift[0],lat=adcp['lat'],constitnames=clist,out_style=None)
+                osnr=nsnr
+                clist=oout2['nameu'][oout2['snr']>2]
+                nsnr=len(clist)
+
+            
+            print('T_tide mod')
+            mout=ttide.t_tide(mu+1j*mv,dt=60.0/3600.0,stime=timeshift[0],lat=model['lat'],out_style=None)
+            osnr=0
+            clist=mout['nameu'][mout['snr']>2]
+            nsnr=len(clist)
+            while(osnr!=nsnr):
+                #print('looping')
+                mout2=ttide.t_tide(mu+1j*mv,dt=60.0/3600.0,stime=timeshift[0],lat=model['lat'],constitnames=clist,out_style=None)
+                osnr=nsnr
+                clist=mout2['nameu'][mout2['snr']>2]
+                nsnr=len(clist)
+            
+            nidx=np.isnan(oout2['xres'])
+            r2v=residual_stats(np.imag(mout2['xres'])[~nidx],np.imag(oout2['xres'])[~nidx])
+            r2u=residual_stats(np.real(mout2['xres'])[~nidx],np.real(oout2['xres'])[~nidx])
+            
+
+            #save it all here
+            rstats=pd.DataFrame([r1u,r1v,r2u,r2v],index=['u_stats','v_stats','u_res_stats','v_res_stats'])
+            rstats.to_csv('{}ADCP_{}_timeseries_stats_at_{}m.csv'.format(loadpath,adcp['metadata']['ADCP_number'],level))
+            
+            df=pd.DataFrame([timeshift,ou,ov,mu,mv,np.real(oout2(timeshift)),np.imag(oout2(timeshift)),np.real(mout2(timeshift)),np.imag(mout2(timeshift))],index=['time','obs_u','obs_v','mod_u','mod_v','obs_u_res','obs_v_res','mod_u_res','mod_v_res']).T
+            df.to_csv('{}ADCP_{}_timeseries_at_{}m.csv'.format(loadpath,adcp['metadata']['ADCP_number'],level),na_rep='NaN')
+            
+            ostr,odf='{}ADCP_{}_obs_ttide_output_at_{}m.txt'.format(loadpath,adcp['metadata']['ADCP_number'],level),'{}ADCP_{}_obs_ttide_tidecon_at_{}m.csv'.format(loadpath,adcp['metadata']['ADCP_number'],level)        
+            oout2.pandas_style(ostr,odf)
+
+            mstr,mdf='{}ADCP_{}_mod_ttide_output_at_{}m.txt'.format(loadpath,adcp['metadata']['ADCP_number'],level),'{}ADCP_{}_mod_ttide_tidecon_at_{}m.csv'.format(loadpath,adcp['metadata']['ADCP_number'],level)
+            mout2.pandas_style(mstr,mdf)
+
+        except:
+            print('could not calculate level {}'.format(level))
+
+
---
+++ extract_adcp_station.py
@@ -0,0 +1,129 @@
+from __future__ import division,print_function
+import matplotlib as mpl
+import scipy as sp
+from folderpath import *
+from datatools import *
+from gridtools import *
+from plottools import *
+from projtools import *
+import interptools as ipt
+import matplotlib.tri as mplt
+import matplotlib.pyplot as plt
+#from mpl_toolkits.basemap import Basemap
+import os as os
+import sys
+np.set_printoptions(precision=8,suppress=True,threshold=np.nan)
+import pandas as pd
+import netCDF4 as n4
+import copy
+import matplotlib.dates as dates
+from collections import OrderedDict
+
+
+# Define names and types of data
+name='sjh_lr_v1_year_origbc_wet_hfx100'
+#name='sjh_hr_v3_year_wet'
+grid='sjh_lr_v1'
+datatype='2d'
+print(name)
+
+### load the .nc file #####
+data = loadnc('/mnt/drive_0/misc/gpscrsync/sjh_lr_v1_year_origbc_wet_hfx100/output/',grid + '_station_timeseries.nc',False)
+#data = loadnc('/fs/vnas_Hdfo/odis/mif001/scratch/sjh_lr_v1_sub/{}/output/'.format(name),grid + '_station_timeseries.nc',False)
+#data = loadnc('/gpfs/fs1/dfo/dfo_odis/yow001/BoF/{}/output/'.format(name),grid + '_station_timeseries.nc',False)
+data['lon']=data['lon']-360
+data['x'],data['y'],data['proj']=lcc(data['lon'],data['lat'])
+
+print('done load')
+
+print('processing time')
+#for j in range(int((len(data['time_JD'])/10000)+1)):
+#    test=np.divide(data['time_second'][j*10000:(j+1)*10000],86400.0)
+#    print(j)
+if 'time_JD' in data.keys():
+    #num=int(len(data['time_JD'])/len(np.arange(0,86400,60)))
+    #ts=np.append(np.arange(data['time_second'][0],86400,60)/86400.0,np.tile(np.arange(0,86400,60)/86400.0,num))
+    #lmin=np.hstack([len(data['time_JD']),len(ts)]).min()-10
+    #data['time']=data['time_JD'][:lmin]+ts[:lmin]+678576
+    data['time']=data['time_JD']+(data['time_second']/86400.0)+678576
+    data['dTimes']=dates.num2date(data['time'])
+    data['Time']=np.array([ct.isoformat(sep=' ')[:19] for ct in data['dTimes']])
+else:
+    data['time']=data['time']+678576
+    data['dTimes']=dates.num2date(data['time'])
+    data['Time']=np.array([ct.isoformat(sep=' ')[:19] for ct in data['dTimes']])
+
+
+print('done time')
+
+filenames=glob.glob('/mnt/drive_1/obs_data/east/adcp/*.npy')
+filenames.sort()
+
+savepath='{}/{}_{}/adcp/{}/'.format(datapath,grid,datatype,name)
+if not os.path.exists(savepath): os.makedirs(savepath)
+
+names=np.array([''.join(data['name_station'][j,:]).strip() for j,n in enumerate(data['name_station'])])
+
+for i,filename in enumerate(filenames):
+    print('='*80)
+    print(i)
+    print(filename)
+    
+    adcp = np.load(filename)
+    adcp = adcp[()]
+    
+    lon=adcp['lon'][0,0]
+    lat=adcp['lat'][0,0]  
+    time=dates.datestr2num(adcp['time']['Times'])
+    
+    #idx=np.argmin(((data['lon']-lon)**2 + (data['lat']-lat)**2))
+    try:
+        idx=np.argwhere(names=='ADCP_{}'.format(adcp['metadata']['ADCP_number']))[0,0]
+    except:
+        continue
+    #expand time window by +-3 hours, should means this works for ctd as well
+    tidx=np.argwhere((data['time']>=(time[0]-(3.0/24.0)))&(data['time']<=(time[-1]+(3.0/24.0))))
+    
+    name=''.join(data['name_station'][idx,:])
+    print(name)
+    if name[5:8]!=str(adcp['metadata']['ADCP_number']):
+        print('Bad ADCP_number match skipping')
+        continue
+    
+    out=OrderedDict()
+    out['time']=data['time'][tidx]
+    out['Time']=data['Time'][tidx]
+    print('Extracted time')
+    
+    out['h']=data['h'][idx]
+    out['zeta']=data['zeta'][tidx,idx]
+    out['ua']=data['ua'][tidx,idx]
+    out['va']=data['va'][tidx,idx]
+    print('Extracted 2d')
+    
+    out['u']=data['u'][tidx,:,idx]
+    out['v']=data['v'][tidx,:,idx]
+    out['ww']=data['ww'][tidx,:,idx]
+    out['temp']=data['temp'][tidx,:,idx]
+    out['salinity']=data['salinity'][tidx,:,idx]
+    print('Extracted 3d')
+    
+    out['siglev']=data['siglev'][:,idx]
+    out['siglay']=data['siglay'][:,idx]
+    out['lon']=data['lon'][idx]
+    out['lat']=data['lat'][idx]
+    out['name_station']=data['name_station'][idx,:]
+    print('Extracted misc')
+    
+    for key in out:
+        out[key]=np.squeeze(out[key])
+    
+    np.save('{}{}_model_ministation.npy'.format(savepath,''.join(data['name_station'][idx,:]).strip()),out)
+    print('Saved')
+
+
+
+
+
+
+
